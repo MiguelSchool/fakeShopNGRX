4fbaa0b1d1f71de41479352f4b94b285625139c7
miguel <miguel-gutierrez@mail.de>
2022-02-08 11:10:30 +0100

    TTD loginComponent:  -> formValidation

--- src/app/auth/components/login/login.component.spec.ts
+++ src/app/auth/components/login/login.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {UserInterface} from "../../../shared/types/UserInterface";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -23,3 +25,55 @@ describe('LoginComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+describe('form validation', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, FormsModule],
+      declarations: [LoginComponent]
+    });
+
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    component.ngOnInit();
+  });
+
+  it('should be form invalid when empty', () => {
+    expect(component.formGroup.valid).toBeFalsy();
+  });
+
+  it('username validity', () => {
+    let username = component.formGroup.controls['username'];
+    expect(username.valid).toBeFalsy();
+  });
+
+  it('username field validity', () => {
+    let username = component.formGroup.controls['username'];
+    let errors = username.errors || {}
+    expect(errors['required']).toBeTruthy();
+
+    username.setValue('johnd');
+    errors = username.errors || {}
+
+    expect(errors['pattern']).toBeTruthy()
+  });
+
+  it('should submitting a form emits a user', () => {
+    expect(component.formGroup.valid).toBeFalsy();
+
+    component.formGroup.controls['username'].setValue('johnd');
+    component.formGroup.controls['password'].setValue('m38rmF$');
+
+    expect(component.formGroup.valid).toBeTruthy();
+
+    let user: UserInterface;
+    component.isLoggedIn$.subscribe((value) => user = value);
+    component.onLogin();
+
+    expect(user.username).toBe('johnd');
+    expect(user.password).toBe('m38rmF$')
+  });
+})
